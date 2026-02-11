de760966e1907a4e7164d63a633bcc8c2f900636
s576ai <s576ai@gmail.com>
2026-02-11 15:56:20 +0900

    process.env가 기존 환경변수와 겹치지 않게 이름 수정

--- month2/book-shop/mariadb.js
+++ month2/book-shop/mariadb.js
@@ -2,12 +2,12 @@ const mariadb = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const conn = mariadb = mariadb.createConnection({
-    host: '127.0.0.1',
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: 'bookshop',
-    dataStrings: true
+const conn = mariadb.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'bookshop'
 });
 
 module.exports = conn;
