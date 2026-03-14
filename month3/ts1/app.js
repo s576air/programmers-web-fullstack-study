a40cf26273cb05c48f5baed772d129fd2543e86e
s576air <s576ai@naver.com>
2026-03-14 20:52:57 +0900

    ts 연습

---
+++ month3/ts1/app.js
@@ -0,0 +1,9 @@
+function logName(name) {
+    console.log(name);
+}
+logName('hello');
+{
+    var name_1 = 1;
+    console.log(name_1);
+    var name_2 = 1;
+}
---
+++ month3/ts1/app.ts
@@ -0,0 +1,15 @@
+function logName(name: string) {
+    console.log(name);
+}
+
+logName('hello');
+
+let student = {
+    name: 'john',
+    score: 100
+};
+
+// student.name = 1; // error
+student.name = 'hi';
+
+let n: number = 1;
