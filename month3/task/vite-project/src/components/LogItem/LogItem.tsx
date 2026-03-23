50e2e13d1cefa7e9ea9c77dd9123205593ff810a
s576air <s576ai@naver.com>
2026-03-23 15:58:24 +0900

    task 3

--- month3/task/vite-project/src/components/LogItem/LogItem.tsx
+++ month3/task/vite-project/src/components/LogItem/LogItem.tsx
@@ -1,6 +1,31 @@
-const LogItem = () => {
+import type { ILogItem } from "../../types";
+import { BsFillPersonFill } from 'react-icons/bs'
+import { author, date, logItemWrap, message } from "./LogItem.css";
+
+type TLogItemProps = {
+  logItem: ILogItem;
+}
+
+const LogItem: React.FC<TLogItemProps> = ({
+  logItem
+}) => {
+  // Date.now()는 impure 함수라 에러가 나는데, 일단 따라치려고 대충 수정
+  const timeOffset = new Date(new Date().getTime() - Number(logItem.logTimestamp));
+
+  const showOffsetTime = `
+  ${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ''}
+  ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ''}
+  ${timeOffset.getSeconds() === 0 ? 'just now' : ''}
+  `;
   return (
-    <div>LogItem</div>
+    <div className={logItemWrap}>
+      <div className={author}>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div className={message}>{logItem.logMessage}</div>
+      <div className={date}>{showOffsetTime}</div>
+    </div>
   )
 }
 
--- month3/task/vite-project/src/store/slices/boardsSlice.ts
+++ month3/task/vite-project/src/store/slices/boardsSlice.ts
@@ -32,6 +32,10 @@ type TDeleteTaskAction = {
     taskId: string;
 }
 
+type TDeleteBoardAction = {
+    boardId: string;
+}
+
 const initialState: TBoardsState = {
     modalActive: false,
     boardArray: [
@@ -86,6 +90,12 @@ const boardsSlice = createSlice({
     reducers: {
         addBoard: (state, {payload}: PayloadAction<TAddBoardAction>) => {
             state.boardArray.push(payload.board);
+        },
+
+        deleteBoard: (state, {payload}: PayloadAction<TDeleteBoardAction>) => {
+            state.boardArray = state.boardArray.filter(
+                board => board.boardId !== payload.boardId
+            )
         },
 
         addList: (state, {payload}: PayloadAction<TAddListAction>) => {
@@ -172,5 +182,5 @@ const boardsSlice = createSlice({
     }
 });
 
-export const { addBoard, addList, addTask, updateTask, deleteTask, deleteList, setModalActive } = boardsSlice.actions;
+export const { addBoard, deleteBoard, addList, addTask, updateTask, deleteTask, deleteList, setModalActive } = boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;
