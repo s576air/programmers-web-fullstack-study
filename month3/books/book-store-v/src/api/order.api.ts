112e7661caf14087fab8fafe9c9ce5015ad50e57
s576air <s576ai@naver.com>
2026-04-05 20:06:09 +0900

    중복 제거

--- month3/books/book-store-v/src/api/order.api.ts
+++ month3/books/book-store-v/src/api/order.api.ts
@@ -1,17 +1,19 @@
 import { type Order, type OrderDetailItem, type OrderSheet } from "../models/order.model";
-import { httpClient } from "./http";
+import { httpClient, requestHandler } from "./http";
+
+// export const order = async (orderData: OrderSheet) => {
+//   const response = await httpClient.post("/orders", orderData);
+//   return response.data;
+// };
 
 export const order = async (orderData: OrderSheet) => {
-  const response = await httpClient.post("/orders", orderData);
-  return response.data;
+  return await requestHandler<OrderSheet>("post", "/orders", orderData);
 };
 
 export const fetchOrders = async () => {
-  const response = await httpClient.get<Order[]>("/orders");
-  return response.data;
+  return await requestHandler<Order[]>("post", "/orders");
 };
 
 export const fetchOrder = async (orderId: number) => {
-  const response = await httpClient.get<OrderDetailItem[]>(`/orders/${orderId}`);
-  return response.data;
+  return await requestHandler<OrderDetailItem[]>("post", `/orders/${orderId}`);
 };
